acfee85019e2ce227ab4a2f3b1d4afbbcd33d3b5
whs <hswongac@gmail.com>
2025-01-06 02:40:27 -0500

    Test

--- src/api/auth.py
+++ src/api/auth.py
@@ -11,13 +11,16 @@ api_key_param = os.environ.get("API_KEY_PARAM_NAME")
 api_key_env = os.environ.get("API_KEY")
 if api_key_param:
     ssm = boto3.client("ssm")
-    api_key = ssm.get_parameter(Name=api_key_param, WithDecryption=True)["Parameter"][
+    api_keys_str = ssm.get_parameter(Name=api_key_param, WithDecryption=True)["Parameter"][
         "Value"
     ]
 elif api_key_env:
-    api_key = api_key_env
+    api_keys_str = api_key_env
 else:
-    api_key = DEFAULT_API_KEYS
+    api_keys_str = DEFAULT_API_KEYS
+
+# Convert comma-separated string to a set of API keys
+api_keys = {key.strip() for key in api_keys_str.split(",")}
 
 security = HTTPBearer()
 
@@ -25,7 +28,7 @@ security = HTTPBearer()
 def api_key_auth(
     credentials: Annotated[HTTPAuthorizationCredentials, Depends(security)],
 ):
-    if credentials.credentials != api_key:
+    if credentials.credentials not in api_keys:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid API Key"
         )
